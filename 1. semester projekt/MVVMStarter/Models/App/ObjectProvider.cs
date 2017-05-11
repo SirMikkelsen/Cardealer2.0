faef572b86ff6c31b31ad4f3056b33057a321237
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2017-05-11 10:47:43 +0200

    ObjectProvider Class

---
+++ 1. semester projekt/MVVMStarter/Models/App/ObjectProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMStarter.Models.App
+{
+    class ObjectProvider
+    {
+        public static Domain.Bil.Catalog BilCatalog
+        {
+            get { return Domain.Bil.Catalog.Instance; }
+        }
+
+        public static Domain.Kunde.Catalog KundeCatalog
+        {
+            get { return Domain.Kunde.Catalog.Instance; }
+        }
+    }
+}
