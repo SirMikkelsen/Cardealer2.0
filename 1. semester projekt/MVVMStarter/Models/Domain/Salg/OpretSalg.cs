0ba87c4f347ddc6d51e8633872dee84f555f69d5
Lasse Andersen <lass5437@edu.easj.dk>
2017-05-15 12:09:58 +0200

    Kommission

--- 1. semester projekt/MVVMStarter/Models/Domain/Salg/OpretSalg.cs
+++ 1. semester projekt/MVVMStarter/Models/Domain/Salg/OpretSalg.cs
@@ -11,21 +11,39 @@ namespace MVVMStarter.Models.Domain.OpretSalg
 
         private int _bilkey;
         private int _kundekey;
+        private double _kommisionPrMåned;
+        private double _salgPrMåned;
+        private double _kommission;
+
+        public double KommissionPrMåned
+        {
+            get { return _kommisionPrMåned; }
+            set { _kommisionPrMåned = value; }
+        }
+
+        public double SalgPrMåned
+        {
+            get { return _salgPrMåned; }
+            set { _salgPrMåned = value; }
+        }
+
+        public double Kommission
+        {
+            get { return _kommission; }
+            set { _kommission = value; }
+        }
 
         private DateTimeOffset _salgsdato;
         private int _slutpris;
 
-        public void Salg()
-        {
-            SetDefaultValues();
-        }
 
         public int BilKey
         {
             get { return _bilkey; }
             set { _bilkey = value; }
-        
+
         }
+
         public int KundeKey
         {
             get { return _kundekey; }
@@ -54,6 +72,29 @@ namespace MVVMStarter.Models.Domain.OpretSalg
 
             _salgsdato = DateTimeOffset.Now;
             _slutpris = 0;
+        }
+
+        public double KommissionBeregning()
+        {
+            if (SalgPrMåned >= 100000)
+            {
+                KommissionPrMåned = SalgPrMåned * 1.05;
+            }
+            else
+            {
+                if ((SalgPrMåned < 100000) && (SalgPrMåned <= 200000))
+                {
+                    KommissionPrMåned = SalgPrMåned * 1.07;
+                }
+                else
+                {
+                    if (SalgPrMåned > 200000)
+                    {
+                        Kommission = SalgPrMåned * 1.10;
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
